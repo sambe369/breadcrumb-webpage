af743b7b2845beca1ae0cc739a8f5c913c7a3d5b
sambe369 <aryal.sambhav369@gmail.com>
2024-01-23 18:43:27 +0545

    react icon deletion

--- src/components/home/section_1.jsx
+++ src/components/home/section_1.jsx
@@ -13,7 +13,7 @@ export const Section_1 = () => {
             <h2><span>Navigating</span> the digital landscape: Your journey of excellence begins here.</h2>
             <p className='my-4'>We simplify navigating the modern IT landscape, providing innovative solutions for seamless integration and expertise, guiding businesses to success in technology.</p>
               <p className="my-4">Join us as we navigate the digital horizon together, empowering your enterprise for sustained growth and technological brilliance.</p>
-              <a href="/contact"><button className='button btn btn-lg mt-5'>Let's Discuss Your Project <FiArrowRightCircle className='right-icon ms-2' /></button></a>
+              <a href="/contact"><button className='button btn btn-lg mt-5'>Let's Discuss Your Project</button></a>
           </div>
           <div className="col-md images">
             <span><img className='img-fluid d-none d-sm-block' src="home 1.svg" alt="" /></span> 
