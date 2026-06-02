2cc5637d55f2a1adcf780be2a8ffe2712d614613
sambe369 <aryal.sambhav369@gmail.com>
2026-06-02 21:29:18 +0545

    email changed in contact page

--- src/components/contact section/Contact1.jsx
+++ src/components/contact section/Contact1.jsx
@@ -107,7 +107,7 @@ export default function Contact1() {
               <ContactItem
                 icon={<Mail className="w-6 h-6" />}
                 label="Email us"
-                value="breadcrumbtechnologies@gmail.com"
+                value="info@breadcrumb.com.np"
               />
               <ContactItem
                 icon={<Phone className="w-6 h-6" />}
